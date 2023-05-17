a6bda21e7b8f64c9751fdc5fb14a9af15035b6d3
Maria Flor Di Piero <mariaflordipiero@gmail.com>
2023-05-16 21:53:55 -0300

    style(css):making home page in css and react

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './Pages/Register';
 import Login from './Pages/Login'
+import Home from './Pages/Home'
 
 
 function AppRouter (){
@@ -9,6 +10,7 @@ function AppRouter (){
         <Routes>
             <Route path="/" element={<Register />}/>
             <Route path="/login" element={<Login />}/>
+            <Route path="/home" element={<Home />}/>
         </Routes>
     </Router>
 }
