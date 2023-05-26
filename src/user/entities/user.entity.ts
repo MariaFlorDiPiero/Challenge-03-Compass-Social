293f27ee6b3b4f2b8db64deeec55cd2c909f0b76
Maria Flor Di Piero <mariaflordipiero@gmail.com>
2023-05-26 18:23:44 -0300

    feat(CRUD):implemented CRUD

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  id?: number;
+  email: string;
+  password: string;
+  name: string;
+  username: string;
+  birthdate?: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { NotFoundException } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.create({ data: createUserDto });
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'User created successfully',
+      data: user,
+    };
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return await this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async remove(id: number): Promise<void> {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    await this.prisma.user.delete({ where: { id } });
+  }
+}
